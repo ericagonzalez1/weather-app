75e7774edccc7f45b4db0b55705e0c2bd3bdd48a
ericagonzalez1 <gonzalezerica1234@yahoo.com>
2023-09-14 16:31:24 -0400

    Removed "console.log" from code

--- src/app.js
+++ src/app.js
@@ -72,15 +72,13 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
